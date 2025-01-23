7114a984dbf33333e6e9ccbbd7bf85603e58c8f0
Vinod Reddy <nutanakalvavinodkumar@gmail.com>
2025-01-23 20:35:51 +0530

    first commit - Variables

---
+++ Variables.py
@@ -0,0 +1,18 @@
+#  # is used to comment in Python
+"""
+This is to comment.
+Multi Line comments can be done using triple quotes
+"""
+
+"""
+Variables do not need to be declared with any particular type.
+type() function is used to identify the data type of a variable.
+String variables can be defined using single and double quotes.
+Variables are case-sensitive.
+casting of a variable can be by using datatype(datavalue) ex: x = str(3), y = float(3)
+"""
+
+x = 5
+X = 99
+print(x)
+print(X)
